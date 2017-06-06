4dcab18936cf24eeec85d172ab125bdcf1a76252
rhapi <zumatec@imac.home>
2017-06-06 13:46:41 +0200

    CCAM version 1.0

---
+++ js/class/medical_act.js
@@ -0,0 +1,49 @@
+class MedicalAct {
+	constructor () {
+		this.dateModif = null;
+		this.code = null;
+		this.nameShort = null;
+		this.nameLong = null;
+		this.notes = null;
+	}
+	
+	dateModifGet () {
+		return this.dateModif;
+	}
+	
+	codeGet () {
+		return this.code;
+	}
+	
+	nameShortGet () {
+		return this.nameShort;
+	}
+	
+	nameLongGet () {
+		return this.nameLong;
+	}
+	
+	notesGet () {
+		return this.notes;
+	}
+	
+	dateModifSet (valNew) {
+		this.dateModif = valNew;
+	}
+	
+	codeSet (valNew) {
+		this.code = valNew;
+	}
+	
+	nameShortSet (valNew) {
+		this.nameShort = valNew;
+	}
+	
+	nameLongSet (valNew) {
+		this.nameLong = valNew;
+	}
+	
+	notesSet (valNew) {
+		this.notes = valNew;
+	}
+}
